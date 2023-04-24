25071f44cb627caab81e8d8d35a006876a350ee3
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-04-24 10:55:05 +0300

    add rating stars for cards

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
 const Product = ({product}) => {
     return(
@@ -15,8 +16,9 @@ const Product = ({product}) => {
                 </a>
                 <Card.Text as="div">
                     <div className="my-3">
-                        {product.rating} from {product.numReviews} reviews
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
                     </div>
+                    
                 </Card.Text>
                 <Card.Text as="h3">
                     ${product.price}
