45fad158067bf6e777b0c3daa4015e21ecd9f598
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-04-24 13:02:06 +0300

    add product page with all info

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Container>
             <Routes>
               <Route path='/' element={<HomeScreen />} exact />
+              <Route path='/product/:id' element={<ProductScreen />} />
             </Routes>
           </Container>
         </main>
