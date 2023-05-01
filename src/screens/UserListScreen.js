61af90b082acbbbb516526cf6c6c6c33be814259
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-05-01 13:21:34 +0300

    create the admin panel

---
+++ src/screens/UserListScreen.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+import {Table, Button, Alert} from 'react-bootstrap';
+import {useDispatch, useSelector} from 'react-redux';
+import Loader from '../components/Loader';
+import { listUsers } from '../actions/userActions';
+
+function UserListScreen() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const userList = useSelector(state => state.userList);
+    const {loading, error, users} = userList;
+
+    const userLogin = useSelector(state => state.userLogin);
+    const {userInfo} = userLogin;
+
+    useEffect(() => {
+        if (userInfo && userInfo.isAdmin) {
+            dispatch(listUsers());
+        } else {
+            navigate('/login');
+        }
+        
+    }, [dispatch, navigate]);
+
+    const deleteHandler = (id) => {
+        console.log(id)
+    }
+    return (
+        <div>
+            <h1>Users</h1>
+            {loading ? <Loader/> : error ? <Alert variant='danger'>{error}</Alert> : (
+                <Table striped bordered hover responsive className='table-sm'>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NAME</th>
+                            <th>EMAIL</th>
+                            <th>ADMIN</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {users.map(user => (
+                            <tr key={user._id}>
+                                <td>{user._id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.isAdmin ? (
+                                    <i className='fas fa-check' style={{color: 'green'}}></i>
+                                ) : (
+                                    <i className='fas fa-times' style={{color: 'red'}}></i>
+                                )}</td>
+                                <td>
+                                    <LinkContainer to={`/admin/user/${user._id}`}>
+                                        <Button variant='light' className='btn-sm'>
+                                            <i className='fas fa-edit'></i>
+                                            Edit
+                                        </Button>
+                                    </LinkContainer>
+                                    <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(user._id)}>
+                                        <i className='fas fa-trash'></i>
+                                        Delete
+                                    </Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            )}
+        </div>
+    );
+}
+
+export default UserListScreen;
